8cccf02630028e9bebb7141d724298daa1fd4645
Premanshc <premanshacademics@gmail.com>
2024-07-06 11:37:35 +0530

    6 july products add

--- src/Products.js
+++ src/Products.js
@@ -1,16 +1,22 @@
 const cardData = [
     { 
         id: 1, 
-        imageSrc: './images/image1.png', 
-        title: 'stainless Steel Water Bottle', 
-        affLink: 'https://www.google.com'
+        imageSrc: 'https://m.media-amazon.com/images/I/71DoXuuNExL._AC_SL1500_.jpg', 
+        title: 'Bag Cinch Set', 
+        affLink: 'https://amzn.to/3xIvXa7'
     },
     { 
         id: 2, 
-        imageSrc: './images/image2.png', 
-        title: 'Card Title 2', 
-        affLink: 'https://www.google.com'
+        imageSrc: 'https://m.media-amazon.com/images/I/71GGyNBcJbL._AC_SL1500_.jpg', 
+        title: 'Hair Clog Remover Tool', 
+        affLink: 'https://amzn.to/3xPDQKT'
     },
+    {
+        id: 3,
+        imageSrc: 'https://m.media-amazon.com/images/I/71p+ep3dgPL._AC_SX679_.jpg',
+        title: 'Cascading Clothes Hangers',
+        affLink: 'https://amzn.to/4bwRct5'
+    }
     // Add more card objects here
   ];
 
